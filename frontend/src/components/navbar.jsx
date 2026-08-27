2fbe4b97eeced5b78bd4c4405a3b755079e0caea
Saim <majesticpainter79@gmail.com>
2026-08-27 13:06:31 +0500

    landing page structure complete

---
+++ frontend/src/components/navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <div>
+      <h2>LeaveFlow</h2>
+    </div>
+  );
+}
+
+export default Navbar;
--- frontend/src/pages/landingPage.jsx
+++ frontend/src/pages/landingPage.jsx
@@ -1,5 +1,38 @@
+import Navbar from "../components/navbar";
+import { useNavigate } from "react-router-dom";
+
 function LandingPage() {
-  return <div>Landing Page</div>;
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <Navbar />
+      <div>
+        <div>
+          <h1>welcome to leaveFlow</h1>
+          <p>apply for your leave through a modern leave portal</p>
+        </div>
+        <div>
+          <button
+            onClick={() => {
+              navigate("/employee/login");
+            }}
+          >
+            <h2>Employee</h2>
+            <p>Manage your leave requests</p>
+          </button>
+          <button
+            onClick={() => {
+              navigate("/manager/login");
+            }}
+          >
+            <h2>Manager</h2>
+            <p>Review and manage leave requests</p>
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default LandingPage;
